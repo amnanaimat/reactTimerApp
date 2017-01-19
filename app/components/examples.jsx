59fa7b31a7c613725b4ed35ceb1e3efb5e034781
amna <amna.haider21@gmail.com>
2017-01-19 19:26:56 +0400

    File updates

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 
 var Examples = (props)=>{
 	return (
-      <h3>Example Component</h3>
+		<div>
+		  <h3>Example</h3>
+		  <p>Welcome to Example Page</p>
+		 </div>
     )
 	
 }
