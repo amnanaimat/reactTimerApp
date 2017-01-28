21744a61009f261a3ea798f572e88d41b54d3faa
amna <amna.haider21@gmail.com>
2017-01-28 16:14:40 +0400

    Add CountDown Test

---
+++ app/tests/components/countdown.test.jsx
@@ -0,0 +1,43 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtil = require('react-addons-test-utils');
+var Countdown = require('Countdown');
+
+describe('Countdown',()=>{
+	it('should Exist',()=>{
+		expect(Countdown).toExist();
+		
+		
+	});
+	describe('handelSetCountDown',()=>{
+		
+		it('should set the timer and state',(done)=>{
+			var countdown = TestUtil.renderIntoDocument(<Countdown />);
+			countdown.handelSetCountDown(10);
+			expect(countdown.state.count).toBe(10);
+			expect(countdown.state.countDownStatus).toBe("started");
+			setTimeout(()=>{
+				
+				expect(countdown.state.count).toBe(9);
+				done();
+			},1001);
+			
+			
+		});
+		
+		it('should not set negative value of timer',(done)=>{
+			var countdown = TestUtil.renderIntoDocument(<Countdown />);
+			countdown.handelSetCountDown(3);
+						setTimeout(()=>{
+				
+				expect(countdown.state.count).toBe(0);
+				done();
+			},3001);
+			
+			
+		});
+	});
+	
+});
